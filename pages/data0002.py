582433970d577d55ea360b05b1babea7acf0f4bb
Anjie Liu <anjieliu@wireless-10-148-26-82.public.utexas.edu>
2024-03-03 10:51:49 -0600

    update data0002.json unique values for each variable

--- pages/data0002.py
+++ pages/data0002.py
@@ -2,6 +2,7 @@ import streamlit as st
 from st_pages import add_page_title
 from utils.page_setup import display_page
 from utils.data_io import read_json, read_cdc_api
+from utils.css_utils import selection_box, markdown_background
 from utils.data_visual import display_dataset
 
 
@@ -11,14 +12,18 @@ display_page(page_info)
 
 
 column_description = page_info["columns"]
+variables = list(column_description.keys())
+
+# variable selection box, description of the selected variable
+option = selection_box('Select to see variable description', variables, index=0)
+markdown_background(f'Variable Description: {column_description[option]}')
 client_args = {"dataset_identifier": "n8mc-b4w4", "limit": 10000, "case_month": "2024-01"}
 
-cols = st.columns(19, gap="small")
-for col in cols:
-    option = col.st.selectbox("How would you like to be contacted?", ("Email", "Home phone", "Mobile phone"), index=None, placeholder="Select contact method...")
-
-with st.expander("See explanation"):
-    st.write("The chart above shows some numbers I picked for you. I rolled actual dice for these, so they're *guaranteed* to be random.")
+st.header("Data Query")
+for i in range(5):
+    cols = st.columns(5, gap="small")
+    for c, key in zip(cols, variables[0+5*i:5+5*i]):
+        option = c.selectbox(key, ("Email", "Home phone", "Mobile phone"), index=None, placeholder="Leave it blank")
 #cdc_api = read_cdc_api(client_args)
 
 #display_dataset(cdc_api)
